623a67ce8043643c23e10725213cb070a1216bc5
caaiyen <ailyn.albores2000@gmail.com>
2020-09-14 11:45:14 +0800

    answer the 4 activities

--- sd208exercise1-3.php
+++ sd208exercise1-3.php
@@ -4,47 +4,32 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SD208Exercises</title>
-    <style>
-        .box {
-            border: solid 1px black;
-            width: 50px;
-            height: 50px;
-            display: inline-block;
-            margin-top: -4px;
-        }
-        .black {
-            background-color: black;
-        }
-        .white {
-            background-color: white;
-        }
-    </style>
 </head>
 <body>
     <?php
-        //exercise 1
-        echo "Harvey D. Aparece";
-        //exercise 2
-        echo "<h2>Chess Board</h2>";
-        $fcolor = "black";
-        $scolor = "white";
-        $c = "";
-        for($counter = 0;$counter < 8;$counter++){   
-            ($counter % 2 != 0)?($fcolor = "white" AND $scolor = "black") : ($fcolor = "black" AND $scolor = "white");
-            for($count = 0;$count < 8;$count++){
-                $c = ($count % 2 == 0)? $fcolor :$scolor;
-                echo "<div class = 'box $c'></div>";
-            }
-            echo "<br>";
-        }
-
-        //exercise 3
-
-
-
-        //exercise 4
+        //
+        $cleanSentence = preg_replace("/[,.!?]/"," ","a set of words that is complete in itself, typically containing a subject and predicate, conveying a statement, question, exclamation, or command, and consisting of a main clause and sometimes one or more subordinate clauses.");
         
-
+        $wordArr = explode(" ",$cleanSentence);
+        $uniqueWords = array();
+        foreach ($wordArr as $word){
+            if(!array_key_exists(strtolower($word),$uniqueWords)){
+                if($word != ""){
+                    echo $word." ".CountExists($word,$wordArr)."<br>";
+                    $uniqueWords[strtolower($word)]= CountExists($word,$wordArr);   
+                }
+            }
+        }
+    
+    function CountExists($value,$arr){
+        $count = 0;
+        foreach ($arr as $val){
+            if($val == $value){
+                $count++;
+            }
+        }
+        return $count;
+    }
     ?>
 </body>
 </html>
--- sd208exercise1-4.php
+++ sd208exercise1-4.php
@@ -4,21 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SD208Exercises</title>
-    <style>
-        .box {
-            border: solid 1px black;
-            width: 50px;
-            height: 50px;
-            display: inline-block;
-            margin-top: -4px;
-        }
-        .black {
-            background-color: black;
-        }
-        .white {
-            background-color: white;
-        }
-    </style>
 </head>
 <body>
     <?php
