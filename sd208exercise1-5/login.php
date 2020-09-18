752031bfe242762e9f3e7d59a6bb12ecf2849cd6
caaiyen <ailyn.albores2000@gmail.com>
2020-09-18 12:05:08 +0800

    adds validation

--- sd208exercise1-5/login.php
+++ sd208exercise1-5/login.php
@@ -7,14 +7,18 @@
 
     if(isset($_POST["email"]) && isset($_POST["password"]) ){
         if(empty($_POST["email"])){
-            array_push($$errors,"ERROR: email should not be empty!");
+            $errors["email"] = "email should not be empty!";
         }
         if(empty($_POST["password"])){
-            array_push($$errors,"ERROR: password should not be empty!");
+            $errors["password"] = "password should not be empty!";
         }
         $_SESSION["email"] = $_POST["email"];
         $_SESSION["password"] = $_POST["password"];
+        if(empty($errors)){
+            header("location: bio.php");
+        }
     }
+
 
 ?>
 
@@ -29,10 +33,12 @@
 <body>
     <div class="background">
         <div class="form_card">
-            <h1>Login</h1>
-          <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+        <h1>Login</h1>
+        <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+                <span class="err"><?php echo (empty($errors["email"]))?"":$errors["email"];?></span><br>
                 <input type="text" name="email" placeholder="Email">
                 <br>
+                <span class="err"><?php echo (empty($errors["password"]))?"":$errors["password"];?></span><br>
                 <input type="password" name="password" placeholder="Password">
                 <br>
                 <input type="submit" class="submitBnt" value="Login">
@@ -41,5 +47,6 @@
         </div>
         
     </div>
+    <script src="./src/main.js"></script>
 </body>
 </html>
