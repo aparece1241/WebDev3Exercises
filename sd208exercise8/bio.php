ec1ce85122a6aa5daef1fb99b0f044ecac2e4423
aparece1241 <apareceh7@gmail.com>
2020-12-09 20:27:51 +0800

    new

---
+++ sd208exercise8/bio.php
@@ -0,0 +1,32 @@
+<?php
+    session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./src/style.css">
+    <title>bio</title>
+</head>
+<body>
+    <div class="header">
+        <h1 style="margin-left: 10px;"></h1>
+    </div>
+    <div class="profile">
+        <div class="profile-items">
+            
+            <img id="avatar" style = "width: 250px; height: 250px;" src="./src/img/.png" alt="avatar" srcset="">
+            
+        </div>
+        
+        <div class="profile-items">
+            <div id="items">
+                <h2></h2>
+                <p></p>
+                <p></p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
